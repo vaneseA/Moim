d33dc7bd1285fb9b39227ecf43555767401ea14f
DK <spol@naver.com>
2022-06-14 10:38:02 +0900

    바인딩 연결/ 로그인 페이지 xml 구현 / id,pw 변수 생성

---
+++ app/src/main/java/com/example/moim/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.moim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.moim.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvent()
+        setValues()
+    }
+
+    fun setupEvent() {
+
+        binding.loginBtn.setOnClickListener {
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+        }
+    }
+
+    fun setValues() {
+
+    }
+
+}
