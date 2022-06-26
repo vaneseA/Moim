b4077342676b1acae1dfe33dbb85d354de6e3f30
DK <spol@naver.com>
2022-06-26 20:43:53 +0900

    MainActivity.kt에서 Adapter 달아 주면서 화면 송출

--- app/src/main/java/com/example/moim/MainActivity.kt
+++ app/src/main/java/com/example/moim/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.moim
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.example.moim.adpaters.MainViewPagerAdapter
 import com.example.moim.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var mAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
         setupEvents()
         setValues()
     }
@@ -19,6 +22,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        binding.mainViewPager.adapter = mAdapter
 
     }
 
