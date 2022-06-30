775bc4e14a31dbb5fd0047199326287c3cef937f
DK <spol@naver.com>
2022-06-30 19:55:53 +0900

    actionbar 생성

--- app/src/main/java/com/example/moim/BaseActivity.kt
+++ app/src/main/java/com/example/moim/BaseActivity.kt
@@ -2,7 +2,11 @@ package com.example.moim
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.moim.api.APIList
 import com.example.moim.api.ServerApi
 import retrofit2.Retrofit
@@ -14,9 +18,16 @@ abstract class BaseActivity : AppCompatActivity() {
     lateinit var retrofit : Retrofit
     lateinit var apiList : APIList
 
+    lateinit var titleTxt : TextView
+    lateinit var addBtn : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
 
         retrofit = ServerApi.getRetrofit()
         apiList = retrofit.create(APIList::class.java)
@@ -25,4 +36,18 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun setupEvents()
 
     abstract fun setValues()
+
+    fun setCustomActionBar () {
+        val defActionBar = supportActionBar!!
+
+        defActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val toolbar = defActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
+
+        titleTxt = defActionBar.customView.findViewById(R.id.titleTxt)
+        addBtn = defActionBar.customView.findViewById(R.id.addBtn)
+
+    }
 }
---
+++ app/src/main/java/com/example/moim/ui/menu/NavigationDrawer.kt
@@ -0,0 +1,12 @@
+package com.example.moim.ui.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moim.R
+
+class NavigationDrawer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_drawer)
+    }
+}
