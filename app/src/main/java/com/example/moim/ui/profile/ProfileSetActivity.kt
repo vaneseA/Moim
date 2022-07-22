1b92fee1afc2f474d4f82e07cb374c59d8d1f51e
DK <spol@naver.com>
2022-07-22 20:50:44 +0900

    개인 프로필 셋 이미지, 닉네임 표현

--- app/src/main/java/com/example/moim/ui/profile/ProfileSetActivity.kt
+++ app/src/main/java/com/example/moim/ui/profile/ProfileSetActivity.kt
@@ -2,9 +2,11 @@ package com.example.moim.ui.profile
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.moim.BaseActivity
 import com.example.moim.R
 import com.example.moim.databinding.ActivityProfileSetBinding
+import com.example.moim.utils.GlobalData
 
 class ProfileSetActivity : BaseActivity() {
     lateinit var binding:ActivityProfileSetBinding
@@ -21,7 +23,10 @@ class ProfileSetActivity : BaseActivity() {
 
     override fun setValues() {
 
-
+        Glide.with(mContext)
+            .load(GlobalData.loginUser!!.profileImg)
+            .into(binding.profileImg)
+        binding.nicknameTxt.hint = GlobalData.loginUser!!.nickname
 
     }
 }
