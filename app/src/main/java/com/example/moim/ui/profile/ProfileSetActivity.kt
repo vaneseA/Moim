e992b1ba49b143170afabb8276f117895d591bd0
DK <spol@naver.com>
2022-07-22 21:37:48 +0900

    사진 url 가져오기까지 진행

--- app/src/main/java/com/example/moim/ui/profile/ProfileSetActivity.kt
+++ app/src/main/java/com/example/moim/ui/profile/ProfileSetActivity.kt
@@ -1,9 +1,13 @@
 package com.example.moim.ui.profile
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.moim.BaseActivity
@@ -11,6 +15,8 @@ import com.example.moim.R
 import com.example.moim.databinding.ActivityProfileSetBinding
 import com.example.moim.models.BasicResponse
 import com.example.moim.utils.GlobalData
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.a20220530_keepthetime.dialogs.CustomAlertDialog
 import org.json.JSONObject
 import retrofit2.Call
@@ -31,9 +37,30 @@ class ProfileSetActivity : BaseActivity() {
         binding.profileImg.setOnClickListener {
 //            갤러리를 개발자가 이용 : 유저 허락을 받아야한다. => 권한 세팅
 //            TedPermission 라이브러리
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                  권한 Ok
+                    val myIntent = Intent()
+
+//            갤러리로 사진을 가지러 이동(추가작업) => Intent (4)
+                    myIntent.action = Intent.ACTION_PICK
+                    myIntent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+
+                    startForResult.launch(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    권한이 Denied
+
+                }
+
+            }
 
 //            권한이 OK 일때
-//            갤러리로 사진을 가지러 이동(추가작업) => Intent (3) / (4) 결합
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check()
 
         }
 
@@ -86,5 +113,19 @@ class ProfileSetActivity : BaseActivity() {
             .load(GlobalData.loginUser!!.profileImg)
             .into(binding.profileImg)
         binding.nicknameEdt.hint = GlobalData.loginUser!!.nickname
+    }
+
+    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+//            어떤 사진을 골랏는지? 파악해보자
+//            임시 : 고른 사진을 profileImg에 바로 적용만 (서버전송 X)
+
+//            data? => 이전 화면이 넘겨준 intent
+//            data?.data => 선택한 사진이 들어있는 경로 정보 (Uri)
+            val dataUri = it.data?.data
+
+//            Uri -> 이미지뷰의 사진 (GLide)
+            Glide.with(mContext).load(dataUri).into(binding.profileImg)
+        }
     }
 }
